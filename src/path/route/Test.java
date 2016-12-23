221f8edb162d1567f00d26724595ae5b98c7eeee
CalmAmity <pedaalemmerzak+github@gmail.com>
2016-12-23 14:55:59 +0100

    Test class for route search now uses realistic cost values for generated graph

--- src/path/route/Test.java
+++ src/path/route/Test.java
@@ -21,7 +21,14 @@ public class Test {
 			while ((double) location.getConnections().size() / 5 < Math.random()) {
 				// Time to create a new link between this node and a random other node.
 				int otherLocationIndex = (int) ((double) locations.size() * Math.random());
-				location.link(locations.get(otherLocationIndex), Math.random() * 10);
+				
+				// Determine the actual distance between the two locations.
+				double actualDistance = location.point.euclideanDistanceTo(locations.get(otherLocationIndex).point);
+				// Create a random number between 0.8 and 1.2 to randomise the cost of the action while still taking the actual distance into account.
+				double randomisedDistanceModifier = .8 + (Math.random() * .4);
+				
+				// Link the two locations together with the randomly modified actual distance as the cost value.
+				location.link(locations.get(otherLocationIndex), actualDistance * randomisedDistanceModifier);
 			}
 		}
 		
