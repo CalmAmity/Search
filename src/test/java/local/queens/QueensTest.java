16038804d4c80c1cd1cfad00dd844b9bc1376f71
CalmAmity <pedaalemmerzak+github@gmail.com>
2017-06-22 17:07:59 +0200

    More tests!

--- src/test/java/local/queens/QueensTest.java
+++ src/test/java/local/queens/QueensTest.java
@@ -1,20 +1,11 @@
 package local.queens;
 
-import java.util.Collection;
-import java.util.stream.Collectors;
-
-import core.Action;
 import core.Heuristic;
 import local.hillclimbing.RandomRestart;
 import local.hillclimbing.SteepestAscent;
-import org.junit.Assert;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class QueensTest {
-	private static Logger log = LoggerFactory.getLogger(QueensTest.class);
-	
 	@Test
 	public void randomRestart() {
 		Heuristic<State> heuristic = new NumberOfClashesHeuristic();
@@ -28,38 +19,5 @@ public class QueensTest {
 		Heuristic<State> heuristic = new NumberOfClashesHeuristic();
 		SteepestAscent<State> climb = new SteepestAscent<>(state, heuristic, 100);
 		climb.run();
-	}
-	
-	@Test
-	public void availableActions() {
-		State state1 = new State(new int[]{0, 1, 2});
-		// Determine the set of states reachable from this one.
-		Collection<State> statesReachableFrom1 = state1.determineAvailableActions().stream().map(Action::getResultingState).collect(Collectors.toList());
-		// Check for a number of states that should be present in the resulting set.
-		Assert.assertTrue(statesReachableFrom1.contains(new State(new int[]{2, 1, 2})));
-		Assert.assertTrue(statesReachableFrom1.contains(new State(new int[]{0, 2, 2})));
-		Assert.assertTrue(statesReachableFrom1.contains(new State(new int[]{0, 1, 1})));
-		// Check for a number of states that should *not* be present in the resulting set.
-		Assert.assertFalse(statesReachableFrom1.contains(new State(new int[]{0, 1, 2})));
-		Assert.assertFalse(statesReachableFrom1.contains(new State(new int[]{1, 2, 2})));
-		Assert.assertFalse(statesReachableFrom1.contains(new State(new int[]{2, 1, 0})));
-		
-		// Check the expected number of available actions from states in the default 8x8 space.
-		State state2 = new State(8);
-		Assert.assertEquals(56, state2.determineAvailableActions().size());
-	}
-	
-	@Test
-	public void testGoalDetection() {
-		for (int testIteration = 0; testIteration < 10; testIteration++) {
-			// Create an 8x8 state at random and check it.
-			State state = new State(8);
-			log.debug(state.toString());
-			log.debug("Is " + (state.isGoalState() ? "" : "not ") + "a goal state");
-		}
-		
-		// Create a goal state and check it.
-		State state = new State(new int[]{4, 2, 0, 6, 1, 7, 5, 3});
-		Assert.assertTrue(state.isGoalState());
 	}
 }
---
+++ src/test/java/local/queens/StateTest.java
@@ -0,0 +1,60 @@
+package local.queens;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import core.Action;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StateTest {
+	private static Logger log = LoggerFactory.getLogger(StateTest.class);
+	
+	@Test
+	public void isGoalState() {
+		for (int testIteration = 0; testIteration < 10; testIteration++) {
+			// Create an 8x8 state at random and check it.
+			State state = new State(8);
+			log.debug(state.toString());
+			log.debug("Is " + (state.isGoalState() ? "" : "not ") + "a goal state");
+		}
+		
+		// Create a goal state and check it.
+		State state = new State(new int[]{4, 2, 0, 6, 1, 7, 5, 3});
+		Assert.assertTrue(state.isGoalState());
+	}
+	
+	@Test
+	public void queensAreClashing() {
+		State state = new State(new int[]{0, 1, 2, 0, 1, 2});
+		Assert.assertTrue(state.queensAreClashing(0, 1));
+		Assert.assertTrue(state.queensAreClashing(2, 1));
+		Assert.assertTrue(state.queensAreClashing(1, 4));
+		Assert.assertTrue(state.queensAreClashing(3, 5));
+		Assert.assertFalse(state.queensAreClashing(0, 4));
+		Assert.assertFalse(state.queensAreClashing(5, 1));
+		Assert.assertFalse(state.queensAreClashing(2, 4));
+		Assert.assertFalse(state.queensAreClashing(1, 3));
+	}
+	
+	@Test
+	public void availableActions() {
+		State state1 = new State(new int[]{0, 1, 2});
+		// Determine the set of states reachable from this one.
+		Collection<State> statesReachableFrom1 = state1.determineAvailableActions().stream().map(Action::getResultingState).collect(Collectors.toList());
+		// Check for a number of states that should be present in the resulting set.
+		Assert.assertTrue(statesReachableFrom1.contains(new State(new int[]{2, 1, 2})));
+		Assert.assertTrue(statesReachableFrom1.contains(new State(new int[]{0, 2, 2})));
+		Assert.assertTrue(statesReachableFrom1.contains(new State(new int[]{0, 1, 1})));
+		// Check for a number of states that should *not* be present in the resulting set.
+		Assert.assertFalse(statesReachableFrom1.contains(new State(new int[]{0, 1, 2})));
+		Assert.assertFalse(statesReachableFrom1.contains(new State(new int[]{1, 2, 2})));
+		Assert.assertFalse(statesReachableFrom1.contains(new State(new int[]{2, 1, 0})));
+		
+		// Check the expected number of available actions from states in the default 8x8 space.
+		State state2 = new State(8);
+		Assert.assertEquals(56, state2.determineAvailableActions().size());
+	}
+}
